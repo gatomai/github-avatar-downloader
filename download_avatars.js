24e4a43cc0f6563dd57504c6f2469f722d53af40
Sai Gautam <sai.gautam@gmail.com>
2018-07-09 21:52:13 +0000

    Added functionality to display each avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -9,11 +9,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
           'User-Agent': 'request',
-          'Authorization': secret.token
+          'Authorization': secret.GITHUB_TOKEN
         }
       };
     
       request(options, function(err, res, body) {
         cb(err, body);
       });
+      
     }
+
+    getRepoContributors("jquery", "jquery", function(err, result) {
+        console.log("Errors:", err);
+        // console.log("Result:", result);
+        var jres = JSON.parse(result);
+        // console.log(jres);
+        for (var prop in jres) {
+            console.log("Key:" + prop);
+            console.log("Value:" + jres[prop].avatar_url);
+        }
+
+      });
