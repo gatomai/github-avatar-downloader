c27120baabc328eab52e73e324327158ecb312ae
Sai Gautam <sai.gautam@gmail.com>
2018-07-09 20:59:17 +0000

    Added Authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,19 @@
 var request = require('request');
+var secret = require('./secret.js');
+
 
 console.log('Welcome to the GitHub Avatar Downloader');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
-  }
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-  });
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent': 'request'
+          'Authorization': secret.GITHUB_TOKEN
+        }
+      };
+    
+      request(options, function(err, res, body) {
+        cb(err, body);
+      });
+    }
